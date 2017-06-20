f9083ab59ffc86be502b4bef0fc1fe2085cacf6c
fvvk <fvvk@users.noreply.github.com>
2017-06-20 16:20:21 +0200

    Add files via upload

---
+++ read2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import codecs
+import re
+import string
+file = codecs.open('file.txt', 'r', 'utf-8')
+text = file.read().lower()
+file.close()
+
+
+word_list = re.findall(r'\b[a-zöäü]{4,20}\b', text)
+
+word_freq = {}
+
+for word in word_list:
+
+    word_freq[word] = word_freq.get(word, 0) + 1
+	
+result = sorted(word_freq.items(), key = lambda x:x[1], reverse = True)
+for word,freq in result:
+    print ("%-20s %d" % (word, freq))
